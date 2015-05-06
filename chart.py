39006c38a7136d3d3f13da57803cc65e0a75b203
14k <bevennyamande@gmail.com>
2015-05-07 00:32:55 +0200

    Update chart.py

--- chart.py
+++ chart.py
@@ -1,6 +1,12 @@
 import pygal
 
 bc = pygal.Bar()
+
+# set the title for the graph
 bc.title = 'Github Users in Africa by Country'
+
+#add the country as well as the users and percentage
 bc.add()
+
+# save the chart to a file
 bc.render_to_file('chart.svg')
